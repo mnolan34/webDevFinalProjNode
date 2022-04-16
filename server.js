3b8d1493d345d86c20baaa288bb00e71ed7532e6
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-15 23:32:29 -0400

    Updated Server

--- server.js
+++ server.js
@@ -2,25 +2,34 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from 'cors';
 
-import helloController from "./controllers/hello-controller.js";
+//Import controllers
 import userController from "./controllers/user-controller.js";
-import tuitController from "./controllers/tuit-controller.js";
 
+//Connect to Database
 const PROTOCOL = "mongodb+srv";
-//const DB_USERNAME = process.env.USERNAME;
-//const DB_PASSWORD = process.env.PASSWORD;
-//const HOST = "";
-//const DB_NAME = ""
-//const DB_QUERY = "retryWrites=true&w=majority";
-//const connectionString = `${PROTOCOL}://webDev:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
-//mongoose.connect(connectionString);
+const DB_USERNAME = process.env.USERNAME;
+const DB_PASSWORD = process.env.PASSWORD;
+const HOST = process.env.DB_HOST;
+const DB_NAME = "netflicks";
+const DB_QUERY = "retryWrites=true&w=majority";
+const connectionString = `${PROTOCOL}://webDev:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
+mongoose.connect(connectionString);
 
+//Build the App
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CORS_ORIGIN
+}));
 app.use(express.json());
+//TODO Doublecheck below notation
+app.use(express.session());
 
-helloController(app);
+
 userController(app);
-tuitController(app);
+
+app.get('/', (req, res) =>
+    res.send('Welcome!'));
+
 
 app.listen(process.env.PORT || 4000);
