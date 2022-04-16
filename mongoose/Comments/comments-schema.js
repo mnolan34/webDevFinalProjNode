9cad178cbdba1cf2dcee4639426ff08767be4144
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-16 11:41:11 -0400

    Fixed mongoose schema collections->collection

--- mongoose/Comments/comments-schema.js
+++ mongoose/Comments/comments-schema.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const commentsSchema = new mongoose.Schema({
     body: String,
     postedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
+    postedOn: Date,
     movie: {type: Schema.Types.ObjectId, ref: "MovieModel"}
-}, {collections: "comments"});
+}, {collection: 'comments'});
 
 export default commentsSchema;
--- mongoose/Likes/likes-schema.js
+++ mongoose/Likes/likes-schema.js
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 const likesSchema = new mongoose.Schema({
     user: {type: Schema.Types.ObjectId, ref: "UserModel"},
     movie: {type: Schema.Types.ObjectId, ref: "MovieModel"}
-}, {collections: "likes"});
+}, {collection: "likes"});
 
 export default likesSchema;
