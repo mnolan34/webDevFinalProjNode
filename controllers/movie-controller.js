396d3ca9b3b4c5cd72a24f90a94c7d075f658e4b
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-16 23:18:34 -0400

    Added professors' movie dao and controller

---
+++ controllers/movie-controller.js
@@ -0,0 +1,19 @@
+const movieDao = require('../dao/movie-dao.js');
+
+const likeMovie = async (req, res) => {
+    const movie = req.body
+    const actualMovie = await movieDao.likeMovie(movie)
+    res.json(actualMovie)
+}
+const dislikeMovie = async () => {}
+const findMovieByImdbID = async (req, res) => {
+    const imdbID = req.params.imdbID
+    const movie = await movieDao.findMovieByImdbID(imdbID)
+    res.json(movie)
+}
+
+module.exports = (app) => {
+    app.post('/api/likes', likeMovie)
+    app.post('/api/dislikes', dislikeMovie)
+    app.get('/api/movies/:imdbID', findMovieByImdbID)
+}
