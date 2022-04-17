d118cc18ac72b35fee57543f2016f8c7f0fe233d
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-16 20:46:10 -0400

    Update Server to fix heroku issue

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from 'cors';
 
-//Import controllers
 import userController from "./controllers/user-controller.js";
 
-//Connect to Database
 const PROTOCOL = "mongodb+srv";
 const DB_USERNAME = process.env.USERNAME;
 const DB_PASSWORD = process.env.PASSWORD;
@@ -15,7 +13,6 @@ const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
 mongoose.connect(connectionString);
 
-//Build the App
 const app = express();
 app.use(cors());
 app.use(express.json());
