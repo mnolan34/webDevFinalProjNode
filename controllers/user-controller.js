124a0444854571293014580c59c15e3146b0510e
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-21 17:12:45 -0400

    Added findUserType functions

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,13 @@
 import * as usersDao from "../dao/users-dao.js";
+import user from "../mongoose/Users/user-schema";
 import {findUserByType} from "../dao/users-dao.js";
 
 const userController = (app) => {
     app.get('/api/users', findAllUsers);
-    app.get('/api/users/:typeOfUser', findUserByType);
+    //app.get('/api/users/:typeOfUser', findUserByType);
     app.get('/api/users/:uid', findUserById);
+    app.get('/api/users/:uid', findIfUserAdmin);
+    app.get('/api/users/:uid', findIfUserCritic);
     app.post('/api/users', createUser);
     app.delete('/api/users/:uid', deleteUser);
     app.put('/api/users/:uid', updateUser);
@@ -14,14 +17,28 @@ const findAllUsers = async (req, res) => {
     return await usersDao.findAllUsers().then(user => res.json(user));
 }
 
+/*
 const findUsersByType = async (req, res) => {
     const userType = req.params.userType;
     return await usersDao.findUserByType(userType).then(user => res.json(user));
 }
+ */
 
 const findUserById = async (req, res) => {
     const userId = req.params.uid;
     return await usersDao.findUserById(userId).then(user => res.json(user));
+}
+
+const findIfUserCritic = async (req, res) => {
+    const userId = req.params.uid;
+    const pulledUser = usersDao.findUserById(userId).then(user=>user);
+    return await res.json(pulledUser.isCritic());
+}
+
+const findIfUserAdmin = async(req, res) => {
+    const userId = req.params.uid;
+    const pulledUser = usersDao.findUserById(userId).then(user=>user);
+    return await res.json(pulledUser.isAdmin());
 }
 
 const createUser = async (req, res) => {
--- dao/users-dao.js
+++ dao/users-dao.js
@@ -10,6 +10,7 @@ export const updateUser = (uid, user) => userModel.updateOne({ _id: uid }, { $se
 export const findUserByLogin = (userName, password) => userModel.findOne({userName, password});
 export const findUserByUsername = (userName) => userModel.findOne(userName);
 export const findUserByType = (type) => userModel.find(type);
+export const findUserType = (uid) => userModel.findOne({_id:uid});
 
 export default { findAllUsers, findUserById, createUser, deleteUser, updateUser, findUserByLogin, findUserByUsername,
                     findUserByType};
