6edbc349638003b8c3613e027e68b8e23ea87d3a
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-20 21:00:32 -0400

    Updated Mongoose Models as per meeting request

--- mongoose/Bookmarks/bookmarks-schema.js
+++ mongoose/Bookmarks/bookmarks-schema.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const BookmarkSchema = mongoose.Schema(
     {
         movie: { type: mongoose.Schema.Types.ObjectId, ref: "MovieModel" },
-        bookmarkedBy: { type: String, ref: "UserModel" }
+        bookmarkedBy: { type: mongoose.Schema.Types.ObjectId, ref: "UserModel" }
     }, { collection: "bookmarks" }
 );
 
