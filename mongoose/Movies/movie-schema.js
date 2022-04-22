bf84b64952e8ba2fa67d1299f6854d31a2e05ff7
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-22 16:15:39 -0400

    Added yearReleased and parentRating to movieSchema

--- mongoose/Movies/movie-schema.js
+++ mongoose/Movies/movie-schema.js
@@ -6,7 +6,9 @@ const movieSchema = mongoose.Schema({
     omdbID: {type:String, required: true},
     moviePoster: {type: String},
     movieTrailer: {type: JSON},
-    movieDescription: {type: String}
+    movieDescription: {type: String},
+    parentRating: {type: String},
+    yearReleased: {type: String},
 }, { collection: 'movies' });
 
 export default movieSchema;
