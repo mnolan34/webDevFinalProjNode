55a187923e2ff80a27ab658e3b0d3a7980aa354e
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-22 13:14:22 -0400

    Updated Create New Comment per Chok's request

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -2,7 +2,8 @@ import * as commentsDao from "../dao/comments-dao.js";
 
 const createComment = async (req,res) => {
     const newComment = req.body;
-    const insertedComment = await commentsDao.createComment(newComment);
+    const movieId = req.params.mid;
+    const insertedComment = await commentsDao.createComment(movieId, newComment);
     res.json(insertedComment);
 }
 
@@ -38,7 +39,7 @@ const deleteComment = async (req,res) => {
 }
 
 export default(app) =>{
-    app.post('/api/comments', createComment);
+    app.post('/api/title/:titleId/comments', createComment);
     app.get('/api/comments', findAllComments);
     app.get('/api/comments/:mid', findAllCommentsByMovie);
     //TODO: Make sure this is correct URL, not same as Movie
--- dao/comments-dao.js
+++ dao/comments-dao.js
@@ -1,7 +1,7 @@
 import commentsModel from "../mongoose/Comments/comments-model.js";
 
 export const findAllComments = () => commentsModel.find();
-export const createComment = (uid, mid, body) => commentsModel.create(uid, mid, body);
+export const createComment = (mid, commentBody) => commentsModel.create(mid, commentBody);
 export const deleteComment = (cid) => commentsModel.deleteOne({_id: cid});
 export const updateComment = (cid, comment) => commentsModel.updateOne({_id: cid}, {$set: comment});
 export const findAllCommentsByMovie = (mid) => commentsModel.find(mid);
