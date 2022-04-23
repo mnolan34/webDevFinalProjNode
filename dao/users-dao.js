d9bed5590a1f9898faffe4c4a5a62f4c23f3f678
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-23 14:24:47 -0400

    Undid auth-dao as can be stored in the user-dao instead

--- dao/users-dao.js
+++ dao/users-dao.js
@@ -4,21 +4,19 @@
  */
 import userModel from "../mongoose/Users/user-model.js";
 
-
-
 /**
  * Uses UserModel to retrieve all user documents from users collection
  * @returns Promise To be notified when the users are retrieved from
  * database
  */
-export const findAllUsers = () => userModel.find();
+const findAllUsers = () => userModel.find();
 
 /**
  * Uses UserModel to retrieve single user document from users collection
  * @param {string} uid User's primary key
  * @returns Promise To be notified when user is retrieved from the database
  */
-export const findUserById = (uid) =>
+const findUserById = (uid) =>
     userModel.findOne({_id : uid});
 
 /**
@@ -26,7 +24,7 @@ export const findUserById = (uid) =>
  * @param {User} user Instance to be inserted into the database
  * @returns Promise To be notified when user is inserted into the database
  */
-export const createUser = (user) =>
+const createUser = (user) =>
     userModel.create(user);
 
 /**
@@ -34,7 +32,7 @@ export const createUser = (user) =>
  * @param {string} uid Primary key of user to be removed
  * @returns Promise To be notified when user is removed from the database
  */
-export const deleteUser = (uid) =>
+const deleteUser = (uid) =>
     userModel.deleteOne({ _id : uid });
 
 /**
@@ -43,16 +41,24 @@ export const deleteUser = (uid) =>
  * @param {User} user User object containing properties and their new values
  * @returns Promise To be notified when user is updated in the database
  */
-export const updateUser = (uid, user) =>
+const updateUser = (uid, user) =>
     userModel.updateOne(
     { _id: uid },
     { $set: user }
     );
+
+const findUserByUsername = (userName) =>
+    userModel.findOne(userName);
+
+const findUserByType = (type) =>
+    userModel.find(type);
 
 export default {
     findAllUsers,
     findUserById,
     createUser,
     deleteUser,
-    updateUser
+    updateUser,
+    findUserByUsername,
+    findUserByType
 };
