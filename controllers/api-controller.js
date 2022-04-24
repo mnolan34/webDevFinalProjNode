4f6dc56f59194814f70b1b0c160810deac08801c
ChokchaiThanrukprasert <chokchaithan@hotmail.com>
2022-04-24 15:30:07 -0400

    Update movie detail API & add movie schema suggestion

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -12,71 +12,103 @@ import axios from "axios";
  * @returns {Promise<void>}
  */
 const displaySearchDetails = async (req, res) => {
-    // Get query params "q"
-    const searchedMovie = req.query.q;
-    // Run IMDB search API
-    try {
-        // Handle 20x response
-        // Send search request. Throw error if no respond in 15 seconds
-        const searchResponse = await axios.get(`${API_URL}/SearchMovie/${API_KEY}/${searchedMovie}`, {timeout: 15000});
-        // Parse the response from IMDB API
-        const searchResults = searchResponse.data.results;
+  // Get query params "q"
+  const searchedMovie = req.query.q;
+  // Run IMDB search API
+  try {
+    // Handle 20x response
+    // Send search request. Throw error if no respond in 15 seconds
+    const searchResponse = await axios.get(`${API_URL}/SearchMovie/${API_KEY}/${searchedMovie}`, {timeout: 15000});
+    // Parse the response from IMDB API
+    const searchResults = searchResponse.data.results;
 
-        // IMDB API server could be busy and give results: null as response.
-        // Tell client of gateway timeout (504)
-        if (!searchResults) res.sendStatus(504);
-        else {
-            // Otherwise, proceed as usual.
-            const count = searchResults.length;
-            const parsedResult = searchResults.map(title => {return {"_id": title.id, "title": title.title, "image": title.image}});
+    // IMDB API server could be busy and give results: null as response.
+    // Tell client of gateway timeout (504)
+    if (searchResults.errorMessage) res.sendStatus(504);
+    else {
+      // Otherwise, proceed as usual.
+      const count = searchResults.length;
+      const parsedResult = searchResults.map(title => {
+        return {"_id": title.id, "title": title.title, "image": title.image}
+      });
 
-            // Return parsed return
-            res.json({count: count, titles: parsedResult});
-        }
-    }catch (error) {
-        // Handle time out and error response (Not 20x)
-        res.sendStatus(504);
+      // Return parsed return
+      res.json({count: count, titles: parsedResult});
     }
+  } catch (error) {
+    // Handle time out and error response (Not 20x)
+    res.sendStatus(504);
+  }
 }
 
-//This will pull the movie details from the Imdb API
-const pullMovieDetails = async(req, res) => {
-    const chosenMovieID = req.params.imdbID;
-    const movieDetails = axios.get(`${API_URL}/Title/${API_KEY}/${chosenMovieID}/Posters,Images,Trailer,Wikipedia,`);
-    res.json(movieDetails);
-}
-
-const addDetailsToDB = async (req, res) => {
-    const requestedMovie = req.params.imdbID;
-    const movieDetails = await pullMovieDetails(requestedMovie);
-    if(movieDetails){
-        //If movie exists, can pull current movie
-        res.json(movieDetails);
-    }else{
-        //If movie not in DB, send it details, add it
-        const movieInMongoFormat = {
-            movieTitle: {movieDetails}.fullTitle,
-            imdbID: {movieDetails}.id,
-            moviePoster: {movieDetails}.posters,
-            movieTrailer: {movieDetails}.trailer.linkEmbed,
-            movieDescription: {movieDetails}.plot,
-            parentRating: {movieDetails}.contentRating,
-            yearReleased: {movieDetails}.year,
-            similarMovies: {movieDetails}.similars
-        }
-        res.json(await movieDao.createMovie(movieInMongoFormat));
-    }
-}
-
-/*
-const findMoviesBySearch = async(req, res) => {
-    const searchExpression = req.params();
-    const movies = await apiDao.findMoviesBySearch(searchExpression);
-    res.json(movies);
-}
+/**
+ * Pull movie detail from IMDB API.
+ *
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
  */
+const pullMovieDetails = async (req, res) => {
+  const movieID = req.params.titleId;
+  try {
+    // Handle 20x response
+    // Send detail request. Throw error if no respond in 15 seconds
+    const detailResponse = await axios.get(`${API_URL}/Title/${API_KEY}/${movieID}/Trailer,`, {timeout: 15000});
+    // Parse the response from IMDB API
+    const movieData = detailResponse.data;
 
-export default(app) => {
-    app.get(`/api/search`, displaySearchDetails);
-    app.get(`/api/titles/:titleId`, addDetailsToDB);
+    // IMDB API server could be busy and give error message
+    // Tell client of gateway timeout (504)
+    if (movieData.errorMessage) res.sendStatus(504);
+    // Tell the client if movie not found
+    else if (!movieData.id) res.sendStatus(404);
+    else {
+      // Otherwise, proceed as usual.
+      const parsedDetail = {
+        "title": movieData.title,
+        "plot": movieData.plot,
+        "image": movieData.image,
+        "trailer": movieData.trailer ? movieData.trailer.linkEmbed : null,
+        "similars": !movieData.similars? []: movieData.similars.map(similar => {return {
+            "_id": similar.id,
+            "title": similar.title,
+            "image": similar.image
+          }})
+      }
+
+      // Update movie detail in local DB as async side effect.
+      // addDetailsToDB(movieData);
+
+      // Return parsed return
+      res.json(parsedDetail);
+    }
+  } catch (error) {
+    // Handle time out and error response (Not 20x)
+    res.sendStatus(504);
+  }
+}
+
+/**
+ * Add movie detail to local DB
+ *
+ * @param movie detail
+ * @returns {Promise<void>}
+ */
+const addDetailsToDB = async (movieDetails) => {
+  // Check if this movie is already exist in DB to prevent duplication
+
+  // Insert if exist
+  const movieInMongoFormat = {
+      movieTitle: movieDetails.title,
+      imdbID: movieDetails.id,
+      moviePoster: movieDetails.image,
+      movieDescription: movieDetails.plot,
+      yearReleased: movieDetails.year
+    }
+  await movieDao.createMovie(movieInMongoFormat)
+}
+
+export default (app) => {
+  app.get(`/api/search`, displaySearchDetails);
+  app.get(`/api/titles/:titleId`, pullMovieDetails);
 }
--- mongoose/Movies/movie-schema.js
+++ mongoose/Movies/movie-schema.js
@@ -1,15 +1,14 @@
 import mongoose from 'mongoose';
 
-
 const movieSchema = mongoose.Schema({
     movieTitle: {type: String, required: true},
     imdbID: {type:String, required: true},
     moviePoster: {type: String},
-    movieTrailer: {type: JSON},
+    // movieTrailer: {type: JSON},          Difficult to maintain. Consider remove it in next commit
     movieDescription: {type: String},
-    parentRating: {type: String},
-    yearReleased: {type: String},
-    similarMovies: {type: JSON}
+    // parentRating: {type: String},        Difficult to maintain. Consider remove it in next commit
+    yearReleased: {type: String}
+    // similarMovies: {type: JSON}          Difficult to maintain. Consider remove it in next commit
 }, { collection: 'movies' });
 
 export default movieSchema;
