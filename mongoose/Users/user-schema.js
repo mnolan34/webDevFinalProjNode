7b1de57dbc1acc8f2ce28c9a3ebea565762e76c2
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-24 21:33:27 -0400

    add avatar to user schema viet example

--- mongoose/Users/user-schema.js
+++ mongoose/Users/user-schema.js
@@ -2,12 +2,14 @@ import mongoose from 'mongoose';
 const userSchema = mongoose.Schema({
     firstName: String,
     lastName: String,
-    username: {type: String, required: true},
-    emailOrNumber: {type: String, required: true},
-    isCritic: {type: Boolean, required: true},
-    isAdmin: {type: Boolean, required: true},
-    password: {type: String, required: true},
-    dateJoined: {type:Date, default: Date.now()}
-}, {collection: 'users'});
+    avatar: { type: String, default: "https://imgur.com/a/KdBKfPp" },
+    username: { type: String, required: true },
+    emailOrNumber: { type: String, required: true },
+    isCritic: { type: Boolean, required: true },
+    isAdmin: { type: Boolean, required: true },
+    password: { type: String, required: true },
+    dateJoined: { type: Date, default: Date.now() }
+}, { collection: 'users' });
 
 export default userSchema;
+
