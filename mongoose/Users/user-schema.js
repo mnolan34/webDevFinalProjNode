c573abaa1e3c7d0003066abbb4d4de011c068e4d
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-26 08:42:57 -0400

    add avatar image to user schema

--- mongoose/Users/user-schema.js
+++ mongoose/Users/user-schema.js
@@ -2,12 +2,13 @@ import mongoose from 'mongoose';
 const userSchema = mongoose.Schema({
     firstName: String,
     lastName: String,
-    username: {type: String, required: true, unique: true},         // unique will enforce uniqueness on username
-    emailOrNumber: {type: String, required: true, unique: true},    // and email. It will throw error on creating
-    isCritic: {type: Boolean, default: false},                      // duplicate
-    isAdmin: {type: Boolean, default: false},
-    password: {type: String, required: true},
-    dateJoined: {type:Date, default: Date.now}
-}, {collection: 'users'});
+    username: { type: String, required: true, unique: true },         // unique will enforce uniqueness on username
+    emailOrNumber: { type: String, required: true, unique: true },    // and email. It will throw error on creating
+    isCritic: { type: Boolean, default: false },                      // duplicate
+    isAdmin: { type: Boolean, default: false },
+    password: { type: String, required: true },
+    dateJoined: { type: Date, default: Date.now },
+    avatarImage: { type: String, default: "https://imgur.com/a/KdBKfPp" },
+}, { collection: 'users' });
 
 export default userSchema;
